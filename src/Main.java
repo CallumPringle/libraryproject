af2aaeb3c34b2c199e28a4dad9cf7821f281e869
CallumPringle <pringcal@hotmail.com>
2022-01-04 16:13:47 +0000

    deez

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,27 @@
 //currently malding, writing in the bookdetails to the file and using bad variable names
 //added main menu and delete option as well as formatting text file a bit
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+//added the ability to make new files unfortunately and made an abomination of a logon
+import java.io.*;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
+    public static String textName = "";
     public static boolean run = true;
-    public static final String delim = ",";
     public static void main(String[] args) throws IOException {
+        logOn();
+        fileNamesRead();
         while(run == true){
             mainMenu();
         }
     }
     public static String getBookDetails(){
+        final String delim = ",";
         String bookTitle = getInput("what is the title: ");
         String bookISBN = getInput("enter the ISBN: ");
         String bookAuthor = getInput("enter the Author: ");
         String bookGenre = getInput("enter the genre: ");
+        bookDeets ccccccccc = new bookDeets(bookTitle, bookISBN, bookAuthor, bookGenre);
         return (bookTitle + delim + bookISBN + delim + bookAuthor + delim + bookGenre + "\n");
     }
     public static String getInput(String prompt){
@@ -26,11 +29,46 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         return sc.nextLine();
     }
-    public static void createFile() {
+    public static void fileNamesStore(String among){
         try {
-            File myObj = new File("bookstoragefileඞ.txt");
+                FileWriter myWriter = new FileWriter("filenames.txt", false);
+                myWriter.write(among);
+                myWriter.close();
+
+        }  catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+    public static void fileNamesRead(){
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("filenames.txt"));
+            textName = br.readLine();
+            br.close();
+        }  catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+    public static void createNewFile(String sussy) {
+        try {
+            File myObj = new File(sussy + ".txt");
             if (myObj.createNewFile()) {
-                FileWriter myWriter = new FileWriter("bookstoragefileඞ.txt", false);
+                FileWriter myWriter = new FileWriter(sussy + ".txt", false);
+                myWriter.write("Title, ISBN, Author, Genre:\n");
+                myWriter.close();
+                System.out.println("File created " + myObj.getName());
+            }
+        }  catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+    public static void createFile(String sussy) {
+        try {
+            File myObj = new File(sussy);
+            if (myObj.createNewFile()) {
+                FileWriter myWriter = new FileWriter(sussy, false);
                 myWriter.write("Title, ISBN, Author, Genre:\n");
                 myWriter.close();
                 System.out.println("File created " + myObj.getName());
@@ -42,18 +80,19 @@ public class Main {
     }
     public static void writeFile(String deez){
         try {
-            FileWriter myWriter = new FileWriter("bookstoragefileඞ.txt", true);
+            FileWriter myWriter = new FileWriter(textName, true);
             myWriter.write(deez);
             myWriter.close();
-            System.out.println("Successfully wrote to the file.");
+            System.out.println("Successfully wrote to the file" + textName);
         } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
     }
-    public static void deleteFile() throws IOException {
-        File myObj = new File("bookstoragefileඞ.txt");
+    public static void deleteFile() {
+        File myObj = new File(textName);
         myObj.delete();
+        System.out.println(textName + " has been deleted, bussy");
     }
     public static void mainMenu() throws IOException {
         String userChoice = getInput("do you want to add a book, close the program or delete the text file?\nType 'add' to add a book ,'end' to end the program and 'delete' to delete the file : ");
@@ -62,13 +101,46 @@ public class Main {
                 run = false;
                 break;
             case "add":
-                createFile();
+                boolean y = true;
+                while(y == true){
+                String x = getInput("do you want to use the same text file or a different one, \ntype 'new' to use a different one and 'same' to use the same one");
+                switch (x) {
+                    case "same":
+                        createFile(textName);
+                        y = false;
+                        break;
+                    case "new":
+                        String word = getInput("what is the name of the text file you want to enter?");
+                        textName = (word + ".txt");
+                        fileNamesStore(textName);
+                        createNewFile(word);
+                        y = false;
+                        break;
+                    default:
+                        System.out.println("invalid");
+                        break;
+                }
+                }
                 String nuts = getBookDetails();
                 writeFile(nuts);
                 break;
             case "delete":
                 deleteFile();
                 break;
+        }
+    }
+    public static void logOn(){
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("details.txt"));
+            String userName = getInput("enter username");
+            String passWord = getInput("enter password");
+            String line = br.readLine();
+            System.out.println(line);
+            line = br.readLine();
+            br.close();
+        }  catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
         }
     }
 }
